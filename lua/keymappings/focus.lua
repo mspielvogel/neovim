0e2a06b8508206506cb1e1451530e139b6d2caf8
Moritz <m.spielvogel@posteo.net>
2022-09-20 15:52:02 +0200

    change focues keymappings

--- lua/keymappings/focus.lua
+++ lua/keymappings/focus.lua
@@ -1,2 +1,2 @@
-vim.api.nvim_set_keymap('n', '<c-f>', ':FocusMaximise<CR>', { silent = false })
-vim.api.nvim_set_keymap('n', '<c-t>', ':FocusToggle<CR>', { silent = false })
+vim.api.nvim_set_keymap('n', '<space>F', ':FocusMaximise<CR>', { silent = false })
+vim.api.nvim_set_keymap('n', '<space>T', ':FocusToggle<CR>', { silent = false })
